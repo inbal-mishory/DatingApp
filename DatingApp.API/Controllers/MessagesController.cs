06145f8b1af31aca7b2d89793b7bb418d332c358
Inbal Mishory <mishory@gmail.com>
2019-06-05 15:57:43 +0300

    Get user messages works in postman.

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -40,6 +41,23 @@ namespace DatingApp.API.Controllers
             return Ok(messageFromRepo);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetMessagesForUser(int userId, [FromQuery]MessageParams messageParams)
+        {
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) 
+                return Unauthorized();
+            
+            messageParams.UserId = userId;
+            var messagesFromRepo = await _repo.GetMessagesForUser(messageParams);
+
+            var messages = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+
+            Response.AddPagination(messagesFromRepo.CurrentPage, messagesFromRepo.PageSize, 
+                messagesFromRepo.TotalCount, messagesFromRepo.TotalPages);
+            
+            return Ok(messages);
+        }
+
         [HttpPost("")]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
         {
@@ -57,8 +75,10 @@ namespace DatingApp.API.Controllers
 
             _repo.Add(message);
 
+            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
+
             if(await _repo.SaveAll())
-                return CreatedAtRoute("GetMessage", new {id = message.Id}, message);
+                return CreatedAtRoute("GetMessage", new {id = message.Id}, messageToReturn);
 
             throw new Exception("Creating the message failed on save");
         }
